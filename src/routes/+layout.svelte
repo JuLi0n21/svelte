8d7c86519798cb0d06c0c6128dcc7c89544b0d19
ju09279 <julian.schlueter@studmail.w-hs.de>
2023-08-13 23:43:10 +0200

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,34 @@
+<script>
+    import "../app.css";
+    import Navbarlink from "./Navbarlink.svelte"
+    let src = "favicon.png";
+    let links = [
+    {name: "Home" , href:"/" }, 
+    {name: "Products", href:"/about" },
+    {name: "About Us", href:"/contact" },
+    {name: "Contact", href:"/profile" },
+    {name: "Cart", href:"/cart" }];
+
+</script>
+
+<nav class="flex justify-between" >
+<div class="basis-1/5 flex-row">
+    <a href="/">
+    <img {src} alt="logo">
+    </a>
+</div>
+
+<div class="basis-2/4 flex justify-evenly h-30">
+
+ 
+    {#each links as link}
+        <Navbarlink name={link.name} href={link.href} />
+    {/each}
+    </div>
+</nav>
+
+<slot />
+
+<style lang="postcss">
+
+</style>
---
+++ src/routes/Navbarlink.svelte
@@ -0,0 +1,6 @@
+<script> 
+  export let name;
+  export let href;
+</script>
+
+<a {href} class="p-5 bg-sky-500 hover:bg-sky-700 rounded-lg" >{name}</a>
